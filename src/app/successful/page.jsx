7e6299a64f351ae6b3613abb9926c862a46c3d13
scamrader <scamrader@gmail.com>
2026-03-05 11:47:00 +0100

    worked on the successfull page

---
+++ src/app/successful/page.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+export default function ReportLayout() {
+  return (
+    <main className="w-[1280px] mx-auto pt-[40px] pb-[90px]">
+        <div className="flex justify-between items-center">
+        <div className="w-[200px] h-[51px] relative">
+        <Image src="/logo.png" alt="scamrader logo" fill />
+        </div>
+
+        <div className="flex space-x-[12px] items-center">
+            <div className="w-[32px] h-[32px] rounded-full bg-[#D9D9D9]"></div>
+            <span className="text-[#000]">Thanau Abbas</span>
+             <div className="w-[15px] h-[7px] relative">
+            <Image src="/img/arrow_down.png" alt="scamrader logo" fill />
+            </div>
+        </div>
+        </div>
+    
+        <div className="w-[600px] mt-[160px] p-[24px] border border-[#DFDFDF] mx-auto rounded-[24px] flex items-center justify-center">
+            <div className="">
+            <div className="relative w-[120px] h-[120px] mx-auto">
+            <Image src="/img/successfull.png" fill alt="successfull page" />
+            </div>
+
+            <div className="text-center mt-[40px]">
+                <h6 className="text-[#1F2937] font-[500]">Report submitted successfully!</h6>
+                <p className="text-[#4C545F] leading-[1.5] text-[14px]">Thank you for reporting a scam case. This will help protect others.</p>
+                <div className="mt-[24px]">
+                <button className="h-[44px] w-full text-[13px] text-[#fff] text-[#FFFFFF] rounded-[16px] bg-[#0D1117]">Report another account</button>
+                </div>
+            </div> 
+
+            <p className="text-[#0D1117] font-[600] text-[13px] text-center py-[22px]">Back home</p>
+            </div>
+        </div>
+        
+    </main>
+  );
+}
